b60c77a1eac0c81da0924dc293683fde7f60369b
FabricioRocha <FabricioRocha@users.noreply.github.com>
2024-07-17 11:11:58 -0400

    Bugfixing the Login page

--- giftlink-frontend/src/components/LoginPage/LoginPage.js
+++ giftlink-frontend/src/components/LoginPage/LoginPage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect  } from 'react';
 import { useAppContext } from '../../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
+import {urlConfig} from '../../config';
 import './LoginPage.css';
 
 function LoginPage() {
@@ -21,48 +22,44 @@ function LoginPage() {
       }, [navigate])
 
     // insert code here to create handleLogin function and include console.log
-    const handleLogin = async () => {
-        try{
-            //first task
-            const response = await fetch(`/api/auth/login`, {
-                method: 'POST', //Task 7: Set method
-                headers: {
-                    'content-type': 'application/json',
-                    'Authorization': bearerToken ? `Bearer ${bearerToken}` : '', // Include Bearer token if available
-                }, //Task 8: Set headers
+    const handleLogin = async (e) => {
+        e.preventDefault();
                 
-                body: JSON.stringify({    
-                    email: email,
-                    password: password,
-                }) //Task 9: Set body to send user details
-            });
-
-            const json = await response.json();
-            console.log('JSON data: ',json);
+        //first task
+        const response = await fetch(`${urlConfig.backendUrl}/api/auth/login`, {
+            method: 'POST', //Task 7: Set method
+            headers: {
+                'content-type': 'application/json',
+                'Authorization': bearerToken ? `Bearer ${bearerToken}` : '', // Include Bearer token if available
+            }, //Task 8: Set headers
             
-            if (json.authtoken) {
-                //Step 2: Task 2
-                sessionStorage.setItem('auth-token', json.authtoken);
-                sessionStorage.setItem('name', json.userName);
-                sessionStorage.setItem('email', json.userEmail);
-                //Step 2: Task 3
-                setIsLoggedIn(true);
-                //Step 2: Task 4
-                navigate('/app');
-            } else {
-                //Step 2: Task 5
-                document.getElementById("email").value="";
-                document.getElementById("password").value="";
-                setIncorrect("Incorrect password");
-                setTimeout(() => {
-                    setIncorrect("");
-                }, 2000);
-            }
-
-
-          }catch (e) {
-            console.log("Error fetching details: " + e.message);
-        }
+            body: JSON.stringify({    
+                email: email,
+                password: password,
+            }) //Task 9: Set body to send user details
+        });
+        
+        const json = await response.json();
+        console.log('JSON data: ',json);
+        
+        if (json.authtoken) {
+            //Step 2: Task 2
+            sessionStorage.setItem('auth-token', json.authtoken);
+            sessionStorage.setItem('name', json.userName);
+            sessionStorage.setItem('email', json.userEmail);
+            //Step 2: Task 3
+            setIsLoggedIn(true);
+            //Step 2: Task 4
+            navigate('/app');
+        } else {
+            //Step 2: Task 5
+            document.getElementById("email").value="";
+            document.getElementById("password").value="";
+            setIncorrect("Incorrect password");
+            setTimeout(() => {
+                setIncorrect("");
+            }, 2000);
+        }      
     }
 
     return (
